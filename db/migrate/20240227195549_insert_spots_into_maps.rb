08af97f42853929643408169b7c5e722bc6568f4
Vyacheslav Odinokov <vyacheslav.odinokov@outlook.com>
2024-02-27 22:23:02 +0200

    feat: added City spot in map Lorencia

--- db/migrate/20240227195549_insert_spots_into_maps.rb
+++ db/migrate/20240227195549_insert_spots_into_maps.rb
@@ -1,6 +1,9 @@
 class InsertSpotsIntoMaps < ActiveRecord::Migration[7.1]
   def change
     Spot.delete_all
-    Spot.create :name => "Beginner Adventurer's Camp", :map => Map.first # Lorencia
+    lorencia = Map.first
+
+    Spot.create :name => "Lorencia City", :map => lorencia
+    Spot.create :name => "Beginner Adventurer's Camp", :map => lorencia
   end
 end
