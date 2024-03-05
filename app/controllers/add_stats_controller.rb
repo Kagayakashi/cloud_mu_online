f2e6182d2c4b7ce28cfa8f39ceb235b4c1e73fe6
Vyacheslav Odinokov <vyacheslav.odinokov@outlook.com>
2024-03-05 21:10:04 +0200

    feat: added actual stat update after adding

--- app/controllers/add_stats_controller.rb
+++ app/controllers/add_stats_controller.rb
@@ -7,23 +7,25 @@ class AddStatsController < ApplicationController
     stat = params[:stat].to_sym
 
     unless [:strength, :agility, :vitality, :energy].include?(stat)
-      flash.now[:alert] = "You trying to add points to unknown stat."
+      flash.now[:alert] = "You're trying to add points to an unknown stat."
       return render :new, status: :unprocessable_entity
     end
 
     unless amount > 0
-      flash.now[:alert] = "Amount must be a greater than 0."
+      flash.now[:alert] = "Amount must be greater than 0."
       return render :new, status: :unprocessable_entity
     end
 
     unless amount <= active_character.points
-      flash.now[:alert] = "Amount must be a greater than 0 and less than #{ active_character.points }."
+      flash.now[:alert] = "Amount must be greater than 0 and less than #{ active_character.points }."
       return render :new, status: :unprocessable_entity
     end
 
-    # ToDo
-    #points = active_character.points - amount
-    #active_character.update(ponts: points, stat => active_character.{stat} + amount)
+
+    points_left = active_character.points - amount
+    stats_new = active_character[stat] + amount
+
+    active_character.update(points: points_left, stat => stats_new)
 
     redirect_to character_path(active_character), notice: "Added #{ amount } to #{ stat.to_s.capitalize }."
   end
