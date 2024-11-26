65a6f987b57b9dbf063967c9c843f8b5b47425db
Vyacheslav Odinokov <kagayakashi@hotmail.com>
2024-11-26 16:39:58 +0200

    feat: login tests

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class SessionsControllerTest < ActionController::TestCase
+  test "should login with correct credentials" do
+    post :create, params: { user: { email: "user1@example.com", password: "password1" } }
+    assert_redirected_to characters_path
+  end
+
+  test "should not login with wrong credentials" do
+    post :create, params: { user: { email: "unknowm@example.com", password: "unknown" } }
+    assert_redirected_to new_session_path
+    assert_equal "Invalid email or password.", flash[:alert]
+  end
+
+  test "should login and logout" do
+    post :create, params: { user: { email: "user1@example.com", password: "password1" } }
+    assert_redirected_to characters_path
+
+    post :destroy
+    assert_redirected_to root_path
+    assert_equal "You have been logged out.", flash[:notice]
+  end
+end
