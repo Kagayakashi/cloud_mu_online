f1ce387900db904648d6869671b5242ef8e7b958
Vyacheslav Odinokov <kagayakashi@hotmail.com>
2024-12-04 13:00:57 +0200

    fix: error in password controller test

--- test/controllers/passwords_controller_test.rb
+++ test/controllers/passwords_controller_test.rb
@@ -7,13 +7,13 @@ class PasswordsControllerTest < ActionController::TestCase
   end
 
   test "should change password with correct attributes" do
-    post :update, params: { password: "passwordNew", password_confirmation: "passwordNew", password_challenge: "password3" }
+    post :update, params: { user: { password: "passwordNew", password_confirmation: "passwordNew", password_challenge: "password3" } }
     assert_redirected_to edit_password_path
     assert_equal "Your password has been updated successfully.", flash[:notice]
   end
 
   test "should not change password with invalid attributes" do
-    post :update, params: { password: "passwordNew", password_confirmation: "passwordNew", password_challenge: "qqqq" }
+    post :update, params: { user: { password: "passwordNew", password_confirmation: "passwordNew", password_challenge: "qqqq" } }
     assert_response :unprocessable_entity
     assert_template :edit
   end
