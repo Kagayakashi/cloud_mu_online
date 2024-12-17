1cc8ca76de9c2b6cf560737244bf0785aac8672d
Vyacheslav Odinokov <kagayakashi@hotmail.com>
2024-12-17 13:13:56 +0200

    feat: map connection tests

---
+++ test/models/map_connection_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+class MapConnectionTest < ActiveSupport::TestCase
+  def setup
+    @lorencia = maps(:one)
+    @noria = maps(:five)
+    @devias = maps(:four)
+  end
+
+  test "should not be valid with empty all attributes" do
+    connection = MapConnection.new
+    assert_not connection.valid?
+  end
+
+  test "should be valid with filled all attributes" do
+    connection = MapConnection.new(map: @lorencia, connected_map: @noria)
+    assert connection.valid?
+    connection = MapConnection.new(map: @noria, connected_map: @lorencia)
+    assert connection.valid?
+
+    connection = MapConnection.new(map: @lorencia, connected_map: @devias)
+    assert connection.valid?
+    connection = MapConnection.new(map: @devias, connected_map: @lorencia)
+    assert connection.valid?
+  end
+
+  test "should be valid build from map" do
+    connection = @lorencia.map_connections.build(connected_map: @noria)
+    assert connection.valid?
+  end
+
+  test "should be created Dungeon map with connection from Lorencia" do
+    dungeon = @lorencia.connected_maps.create(name: "Dungeon")
+    assert dungeon.valid?
+  end
+end
