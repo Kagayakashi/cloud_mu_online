c9b93ae0894df15f55009e1273d934e5c4ba6b36
Vyacheslav Odinokov <kagayakashi@hotmail.com>
2024-12-17 12:29:43 +0200

    feat: unique name monster type test

--- test/models/monster_type_test.rb
+++ test/models/monster_type_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class MonsterTypeTest < ActiveSupport::TestCase
   def setup
     @spiders_map = maps(:two)
+    @monster_type = monster_types(:spider)
   end
 
   test "should not be valid with empty all attributes" do
@@ -12,6 +13,23 @@ class MonsterTypeTest < ActiveSupport::TestCase
 
   test "should not be valid with name and map only" do
     type = MonsterType.new(name: "I Have Only Name", map: @spiders_map)
+    assert_not type.valid?
+  end
+
+  test "should not be valid with already existing name" do
+    type = MonsterType.new(
+      name: @monster_type.name,
+      level: 5,
+      health: 1000,
+      min_attack: 1,
+      max_attack: 1,
+      attack_rate: 1,
+      defense_rate: 1,
+      defense: 1,
+      experience: 100,
+      spawn_time: 60,
+      map: @spiders_map
+    )
     assert_not type.valid?
   end
 
