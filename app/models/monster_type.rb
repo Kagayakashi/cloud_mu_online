10898587e0e95da2dc9935a992bca6292cbc563b
Vyacheslav Odinokov <kagayakashi@hotmail.com>
2024-12-17 12:56:33 +0200

    feat: extended monster type tests

--- app/models/monster_type.rb
+++ app/models/monster_type.rb
@@ -5,12 +5,15 @@ class MonsterType < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }
   validates :level, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :health, presence: true, numericality: { greater_than_or_equal_to: 1 }
-  validates :min_attack, :max_attack, presence: true, numericality: { greater_than_or_equal_to: 1 }
-  validate :max_attack_greater_than_min_attack
-  validates :attack_rate, :defense_rate, presence: true, numericality: { greater_than_or_equal_to: 1 }
-  validates :defense, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :min_attack, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :max_attack, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :attack_rate, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :defense_rate, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :defense, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :experience, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :spawn_time, presence: true, numericality: { greater_than_or_equal_to: 1 }
+
+  validate :max_attack_greater_than_min_attack
 
   private
 
--- test/models/monster_type_test.rb
+++ test/models/monster_type_test.rb
@@ -4,6 +4,23 @@ class MonsterTypeTest < ActiveSupport::TestCase
   def setup
     @spiders_map = maps(:two)
     @monster_type = monster_types(:spider)
+    @temp_type = MonsterType.new(
+      name: "White Rabbit",
+      level: 5,
+      health: 1000,
+      min_attack: 0,
+      max_attack: 1,
+      attack_rate: 0,
+      defense_rate: 0,
+      defense: 0,
+      experience: 100,
+      spawn_time: 60,
+      map: @spiders_map
+    )
+  end
+
+  test "should be valid precreated monster type" do
+    assert @temp_type.valid?
   end
 
   test "should not be valid with empty all attributes" do
@@ -16,38 +33,54 @@ class MonsterTypeTest < ActiveSupport::TestCase
     assert_not type.valid?
   end
 
-  test "should not be valid with already existing name" do
-    type = MonsterType.new(
-      name: @monster_type.name,
-      level: 5,
-      health: 1000,
-      min_attack: 1,
-      max_attack: 1,
-      attack_rate: 1,
-      defense_rate: 1,
-      defense: 1,
-      experience: 100,
-      spawn_time: 60,
-      map: @spiders_map
-    )
-    assert_not type.valid?
+  test "should not be valid with incorrect attributes existing name" do
+    @temp_type.name = @monster_type.name
+    assert_not @temp_type.valid?
   end
 
-  test "should be valid with filled all attributes" do
-    type = MonsterType.new(
-      name: "White Rabbit",
-      level: 5,
-      health: 1000,
-      min_attack: 1,
-      max_attack: 1,
-      attack_rate: 1,
-      defense_rate: 1,
-      defense: 1,
-      experience: 100,
-      spawn_time: 60,
-      map: @spiders_map
-    )
-    assert type.valid?
+  test "should not be valid with incorrect level" do
+    @temp_type.level = 0
+    assert_not @temp_type.valid?
+    @temp_type.level = -1
+    assert_not @temp_type.valid?
+  end
+
+  test "should not be valid with incorrect health" do
+    @temp_type.health = 0
+    assert_not @temp_type.valid?
+    @temp_type.health = -1
+    assert_not @temp_type.valid?
+  end
+
+  test "should not be valid with incorrect min attack" do
+    @temp_type.min_attack = -1
+    assert_not @temp_type.valid?
+
+    @temp_type.min_attack = 2
+    @temp_type.max_attack = 1
+    assert_not @temp_type.valid?
+  end
+
+  test "should not be valid with incorrect max attack" do
+    @temp_type.max_attack = 0
+    assert_not @temp_type.valid?
+    @temp_type.max_attack = -1
+    assert_not @temp_type.valid?
+  end
+
+  test "should not be valid with incorrect attack rate" do
+    @temp_type.attack_rate = -1
+    assert_not @temp_type.valid?
+  end
+
+  test "should not be valid with incorrect defense rate" do
+    @temp_type.defense_rate = -1
+    assert_not @temp_type.valid?
+  end
+
+  test "should not be valid with incorrect defense" do
+    @temp_type.defense = -1
+    assert_not @temp_type.valid?
   end
 
   test "should be valid build from map" do
