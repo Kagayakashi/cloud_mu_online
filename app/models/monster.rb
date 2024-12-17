bdd788365221719e0dd144d120e8db89ea614583
Vyacheslav Odinokov <kagayakashi@hotmail.com>
2024-12-17 12:27:32 +0200

    feat: monster tests

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -2,11 +2,13 @@ class Monster < ApplicationRecord
   belongs_to :monster_type
   belongs_to :target, class_name: "Characters::Character", foreign_key: :target_id, optional: true
 
-  before_create :set_health
+  before_validation :set_health, if: :new_record?
 
   private
 
   def set_health
-    self.health = monster_type.health
+    if monster_type.present?
+      self.health = monster_type.health
+    end
   end
 end
