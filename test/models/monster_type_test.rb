a879b82135407c974fd5eb0720bc55b458775744
Vyacheslav Odinokov <kagayakashi@hotmail.com>
2024-12-17 12:58:53 +0200

    fix: refactor monster type tests

--- test/models/monster_type_test.rb
+++ test/models/monster_type_test.rb
@@ -3,8 +3,8 @@ require "test_helper"
 class MonsterTypeTest < ActiveSupport::TestCase
   def setup
     @spiders_map = maps(:two)
-    @monster_type = monster_types(:spider)
-    @temp_type = MonsterType.new(
+    @exist_monster_type = monster_types(:spider)
+    @attributes = {
       name: "White Rabbit",
       level: 5,
       health: 1000,
@@ -16,11 +16,12 @@ class MonsterTypeTest < ActiveSupport::TestCase
       experience: 100,
       spawn_time: 60,
       map: @spiders_map
-    )
+    }
+    @monster_type = MonsterType.new(@attributes)
   end
 
   test "should be valid precreated monster type" do
-    assert @temp_type.valid?
+    assert @monster_type.valid?
   end
 
   test "should not be valid with empty all attributes" do
@@ -34,68 +35,57 @@ class MonsterTypeTest < ActiveSupport::TestCase
   end
 
   test "should not be valid with incorrect attributes existing name" do
-    @temp_type.name = @monster_type.name
-    assert_not @temp_type.valid?
+    @monster_type.name = @exist_monster_type.name
+    assert_not @monster_type.valid?
   end
 
   test "should not be valid with incorrect level" do
-    @temp_type.level = 0
-    assert_not @temp_type.valid?
-    @temp_type.level = -1
-    assert_not @temp_type.valid?
+    @monster_type.level = 0
+    assert_not @monster_type.valid?
+    @monster_type.level = -1
+    assert_not @monster_type.valid?
   end
 
   test "should not be valid with incorrect health" do
-    @temp_type.health = 0
-    assert_not @temp_type.valid?
-    @temp_type.health = -1
-    assert_not @temp_type.valid?
+    @monster_type.health = 0
+    assert_not @monster_type.valid?
+    @monster_type.health = -1
+    assert_not @monster_type.valid?
   end
 
   test "should not be valid with incorrect min attack" do
-    @temp_type.min_attack = -1
-    assert_not @temp_type.valid?
+    @monster_type.min_attack = -1
+    assert_not @monster_type.valid?
 
-    @temp_type.min_attack = 2
-    @temp_type.max_attack = 1
-    assert_not @temp_type.valid?
+    @monster_type.min_attack = 2
+    @monster_type.max_attack = 1
+    assert_not @monster_type.valid?
   end
 
   test "should not be valid with incorrect max attack" do
-    @temp_type.max_attack = 0
-    assert_not @temp_type.valid?
-    @temp_type.max_attack = -1
-    assert_not @temp_type.valid?
+    @monster_type.max_attack = 0
+    assert_not @monster_type.valid?
+    @monster_type.max_attack = -1
+    assert_not @monster_type.valid?
   end
 
   test "should not be valid with incorrect attack rate" do
-    @temp_type.attack_rate = -1
-    assert_not @temp_type.valid?
+    @monster_type.attack_rate = -1
+    assert_not @monster_type.valid?
   end
 
   test "should not be valid with incorrect defense rate" do
-    @temp_type.defense_rate = -1
-    assert_not @temp_type.valid?
+    @monster_type.defense_rate = -1
+    assert_not @monster_type.valid?
   end
 
   test "should not be valid with incorrect defense" do
-    @temp_type.defense = -1
-    assert_not @temp_type.valid?
+    @monster_type.defense = -1
+    assert_not @monster_type.valid?
   end
 
   test "should be valid build from map" do
-    type = @spiders_map.monster_types.build(
-      name: "White Rabbit",
-      level: 5,
-      health: 1000,
-      min_attack: 1,
-      max_attack: 1,
-      attack_rate: 1,
-      defense_rate: 1,
-      defense: 1,
-      experience: 100,
-      spawn_time: 60,
-    )
+    type = @spiders_map.monster_types.build(@attributes)
     assert type.valid?
   end
 end
