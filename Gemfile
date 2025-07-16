c7f1ac9f026fe26c06a645057f04f15b79fd4ca0
Vyacheslav Odinokov <kagayakashi@hotmail.com>
2025-07-16 12:48:39 +0300

    fix: update rails to 8.1 and migrate primary

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source "https://rubygems.org"
 ruby "3.3.4"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 8.0.0"
+gem "rails", github: "rails/rails", branch: "main"
 
 # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
 gem "propshaft", "~> 1.1.0"
--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_07_074422) do
+ActiveRecord::Schema[8.1].define(version: 2025_02_07_074422) do
   create_table "characters", force: :cascade do |t|
-    t.string "type", null: false
-    t.string "name", null: false
-    t.integer "level", default: 1, null: false
-    t.integer "health", default: 1, null: false
-    t.integer "max_health", default: 1, null: false
-    t.integer "mana", default: 1, null: false
-    t.integer "max_mana", default: 1, null: false
-    t.integer "attack_rate", default: 1, null: false
-    t.integer "min_attack", default: 1, null: false
-    t.integer "max_attack", default: 1, null: false
-    t.integer "defense_rate", default: 1, null: false
-    t.integer "defense", default: 1, null: false
-    t.integer "strength", default: 1, null: false
+    t.integer "activity", default: 10, null: false
     t.integer "agility", default: 1, null: false
-    t.integer "vitality", default: 1, null: false
+    t.integer "attack_rate", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.integer "defense", default: 1, null: false
+    t.integer "defense_rate", default: 1, null: false
     t.integer "energy", default: 1, null: false
     t.integer "experience", default: 0, null: false
-    t.integer "max_experience", default: 1, null: false
-    t.integer "points", default: 0, null: false
-    t.integer "activity", default: 10, null: false
-    t.integer "max_activity", default: 10, null: false
-    t.datetime "last_restore_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.integer "health", default: 1, null: false
     t.datetime "last_regeneration_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "map_id", null: false
+    t.datetime "last_restore_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.integer "level", default: 1, null: false
     t.integer "location_id"
+    t.integer "mana", default: 1, null: false
+    t.integer "map_id", null: false
+    t.integer "max_activity", default: 10, null: false
+    t.integer "max_attack", default: 1, null: false
+    t.integer "max_experience", default: 1, null: false
+    t.integer "max_health", default: 1, null: false
+    t.integer "max_mana", default: 1, null: false
+    t.integer "min_attack", default: 1, null: false
+    t.string "name", null: false
+    t.integer "points", default: 0, null: false
+    t.integer "strength", default: 1, null: false
+    t.string "type", null: false
+    t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "vitality", default: 1, null: false
     t.index ["location_id"], name: "index_characters_on_location_id"
     t.index ["map_id"], name: "index_characters_on_map_id"
     t.index ["name"], name: "index_characters_on_name", unique: true
@@ -48,19 +48,19 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_07_074422) do
 
   create_table "game_logs", force: :cascade do |t|
     t.integer "character_id", null: false
+    t.datetime "created_at", null: false
     t.text "description"
     t.string "type"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["character_id"], name: "index_game_logs_on_character_id"
   end
 
   create_table "locations", force: :cascade do |t|
-    t.string "name", null: false
     t.string "code", null: false
-    t.boolean "peace", null: false
-    t.integer "map_id", null: false
     t.datetime "created_at", null: false
+    t.integer "map_id", null: false
+    t.string "name", null: false
+    t.boolean "peace", null: false
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_locations_on_code", unique: true
     t.index ["map_id"], name: "index_locations_on_map_id"
@@ -68,31 +68,31 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_07_074422) do
   end
 
   create_table "maps", force: :cascade do |t|
-    t.string "name", null: false
     t.string "code", null: false
-    t.integer "level", default: 1, null: false
     t.datetime "created_at", null: false
+    t.integer "level", default: 1, null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_maps_on_code", unique: true
     t.index ["name"], name: "index_maps_on_name", unique: true
   end
 
   create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
+    t.integer "character_id"
+    t.datetime "created_at", null: false
     t.string "email", null: false
-    t.string "password_digest", null: false
     t.boolean "is_guest", default: true, null: false
     t.datetime "last_login_at"
-    t.datetime "created_at", null: false
+    t.string "password_digest", null: false
     t.datetime "updated_at", null: false
-    t.integer "character_id"
+    t.string "username", null: false
     t.index ["character_id"], name: "index_users_on_character_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
