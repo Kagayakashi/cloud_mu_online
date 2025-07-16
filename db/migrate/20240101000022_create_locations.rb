3d5bdde1a67819b3933cb56a89fa5185aee69e62
Vyacheslav Odinokov <kagayakashi@hotmail.com>
2025-07-16 17:21:13 +0300

    fix: unused parts

--- db/migrate/20240101000022_create_locations.rb
+++ db/migrate/20240101000022_create_locations.rb
@@ -3,7 +3,6 @@ class CreateLocations < ActiveRecord::Migration[7.2]
     create_table :locations do |t|
       t.string :name, null: false, index: { unique: true }
       t.string :code, null: false, index: { unique: true }
-      t.boolean :peace, null: false
 
       t.references :map, foreign_key: { on_delete: :cascade }, null: false
 
