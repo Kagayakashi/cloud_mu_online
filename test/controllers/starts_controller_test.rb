a630ab2748a2fbcd4498416c9642112674e17389
Vyacheslav Odinokov <kagayakashi@hotmail.com>
2025-07-17 16:37:52 +0300

    fix: start controller tests

--- test/controllers/starts_controller_test.rb
+++ test/controllers/starts_controller_test.rb
@@ -1,27 +1,41 @@
 require "test_helper"
 
-class StartsControllerTest < ActionController::TestCase
+class StartsControllerTest < ActionDispatch::IntegrationTest
   test "should create user and character and redirect" do
     assert_difference("Characters::Character.count", 1) do
-      post :create, params: { characters_character: { name: "CreatedDarkKnight3", type: "Characters::DarkKnight" } }
+      post start_path, params: {
+        characters_character: {
+          name: "CreatedDarkKnight3",
+          type: "Characters::DarkKnight"
+        }
+      }
     end
     assert_redirected_to adventure_path
   end
 
-  test "should render new on invalid character type" do
+  test "should not create user and character with invalid type" do
     assert_no_difference "Characters::Character.count" do
-      post :create, params: { characters_character: { name: "InvalidTypeCharacter", type: "Characters::InvalidType" } }
+      post start_path, params: {
+        characters_character: {
+          name: "InvalidTypeCharacter",
+          type: "Characters::InvalidType"
+        }
+      }
     end
 
     assert_response :unprocessable_entity
-    assert_match (/is not a valid character type/), response.body
   end
 
-  test "should render new on empty name" do
+  test "should not create user and character with empty name" do
     assert_no_difference "Characters::Character.count" do
-      post :create, params: { characters_character: { name: "", type: "Characters::DarkKnight" } }
+      post start_path, params: {
+        characters_character: {
+          name: "",
+          type: "Characters::DarkKnight"
+        }
+      }
     end
+
     assert_response :unprocessable_entity
-    assert_match (/can&#39;t be blank/), response.body
   end
 end
