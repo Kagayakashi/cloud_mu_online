9d94caa629a0fe4cacf87ebaf0c34f408472bd41
Vyacheslav Odinokov <kagayakashi@hotmail.com>
2025-07-17 17:54:25 +0300

    fix: session tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,22 +2,21 @@ require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should login with correct credentials" do
-    post session_path, params: { user: { email: "user1@example.com", password: "password1" } }
+    post session_path, params: { email: "user1@example.com", password: "password" }
     assert_redirected_to characters_path
   end
 
   test "should not login with wrong credentials" do
-    post session_path, params: { user: { email: "unknowm@example.com", password: "unknown" } }
+    post session_path, params: { email: "unknowm@example.com", password: "unknown" }
     assert_redirected_to new_session_path
-    assert_equal "Invalid email or password.", flash[:alert]
+    assert_equal "Try another email address or password.", flash[:alert]
   end
 
   test "should login and logout" do
-    post session_path, params: { user: { email: "user1@example.com", password: "password1" } }
+    post session_path, params: { email: "user1@example.com", password: "password" }
     assert_redirected_to characters_path
 
-    post :destroy
+    delete session_path
     assert_redirected_to root_path
-    assert_equal "You have been logged out.", flash[:notice]
   end
 end
